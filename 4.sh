dd0dc0e1e21520cd038d2c0efacb7fdf933c76a8
Pendrag00n <88731722+Pendrag00n@users.noreply.github.com>
2022-11-21 00:47:37 +0000

    Add files via upload

--- 4.sh
+++ 4.sh
@@ -1,2 +1,26 @@
 #!/bin/sh
-echo 4
+if [ "$EUID" -ne 0 ]; then
+  echo ""
+  echo -e "\e[1;31m !EJECUTA ESTE SCRIPT COMO SUPERUSUARIO! \e[0m"
+  echo ""
+exit
+fi
+echo "Creación manual de un usuario"
+echo "Introduzca el username: "
+read username
+if [ -d /home/$username ]; then
+echo "La carpeta del usuario ya existe, borrala primero"
+else
+echo "Introduzca la contraseña: "
+read pass
+largestuid=$(awk -F: '{print $3}' /etc/passwd | sort -n | tail -1)
+nuid=$(echo $largestuid + 1 | bc)
+hashed=$(openssl passwd -6 -salt xyz $pass)
+echo "$username:x:$nuid:$nuid::/home/$username:/bin/bash" >> /etc/passwd
+echo "$username:$hashed:::::::" >> /etc/shadow
+echo "$username:x:$nuid:" >> /etc/group
+mkdir /home/$username
+chown $username:$username /home/$username
+cp -r /etc/skel/. /home/$username
+echo "Usuario creado correctamente"
+fi
--- 8.sh
+++ 8.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-
+# Count number of directories in a directory
+dir=$(pwd)
+echo "Introduce el directorio: "
+read dir
+nd=$(ls -la $dir | grep "^d" | grep -c ^) #MENOS DOS PORQUE CUENTA LOS PUNTOS
+na=$(ls -la $dir | grep "^-" | grep -c ^)
+echo "Numero de directorios: $nd"
+echo "Numero de archivos: $na"
