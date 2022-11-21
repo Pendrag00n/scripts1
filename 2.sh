35da0cc9e4679b352ad31db072753bf49d5a098b
Pendrag00n <88731722+Pendrag00n@users.noreply.github.com>
2022-11-21 18:59:03 +0000

    Add files via upload

--- 2.sh
+++ 2.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+!/bin/sh
+echo -e "\e[1;31m NO EJECUTAR ESTE SCRIPT EN WSL \e[0m"
+echo ""
 if [ "$EUID" -ne 0 ]; then
   echo ""
   echo -e "\e[1;31m !EJECUTA ESTE SCRIPT COMO SUPERUSUARIO! \e[0m"
@@ -8,20 +10,20 @@ fi
 echo "Introduzca una carpeta (O más, separadas por espacios) para crear una copia de seguridad"
 echo "Si no existe la carpeta de backups, se creará"
 read dir
+user=$(logname)
 dirname=$(date +%d%m%Y%H%M%S)
-if [ ! "~/backup/" ]; then
+if [ ! "/home/$user/backup/" ]; then
     echo "Creando directorio de backups..."
-    mkdir -p ~/backup/
+    mkdir -p /home/$user/backup/
 fi
-    echo "La carpeta existe!"
+    echo "La carpeta de backups existe!"
     echo "Comprimiendo..."
-    tar -cvzf ~/backup/backup_$dirname.tar.gz $dir &> /dev/null
+    tar -cpzf /home/$user/backup/backup_$dirname.tar.gz $dir &> /dev/null
     if [ $? -eq 0 ]; then
         echo "Tu copia de seguridad se ha creado en ~/backup"
     else
-        echo "La carpeta que has introducido no existe :("
-        if [ -f ~/backup/backup_$dirname.tar.gz ]; then
-            rm -rf ~/backup/backup_$dirname.tar.gz
+        echo "La carpeta que has introducido no existe o el script no puede leer la carpeta :("
+        if [ -f /home/$user/backup/backup_$dirname.tar.gz ]; then
+            rm -rf /home/$user/backup/backup_$dirname.tar.gz
         fi
     fi
-    
